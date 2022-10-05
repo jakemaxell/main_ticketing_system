5fb41dd4409335dd1f16eaaf1bda0a960dd47208
jakemaxell <jakemaxwell97@gmail.com>
2022-10-05 15:40:42 -0400

    Several Updates

--- ticketing_system/urls.py
+++ ticketing_system/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('tickets/', include('ticket.urls'))
+    path('tickets/', include('ticket.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
