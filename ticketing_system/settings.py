b9a73f079e6186464967ee39a0f46006d8c58304
jakemaxell <jakemaxwell97@gmail.com>
2022-10-05 15:14:15 -0400

    Update settings.py

--- ticketing_system/settings.py
+++ ticketing_system/settings.py
@@ -10,10 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 
 
 # Quick-start development settings - unsuitable for production
