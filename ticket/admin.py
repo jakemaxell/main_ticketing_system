857d30a01cbcf41fbb29eb636ea6accc592f0a79
jakemaxell <jakemaxwell97@gmail.com>
2022-10-05 17:02:43 -0400

    Added the completed column to the admin page

--- ticket/admin.py
+++ ticket/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from . import models
 
 class TicketAdmin(admin.ModelAdmin):
-    list_display = ('title', 'type', 'name', 'created', 'completedDate', 'completedBy')
+    list_display = ('title', 'type', 'name', 'created', 'completedDate', 'completedBy', 'completed')
 
 admin.site.register(models.Ticket, TicketAdmin)
