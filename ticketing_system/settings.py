8a4074115c901d7c2f2db90e20b7161a9437b7c6
Jacob Maxwell <jakemaxwell97@gmail.com>
2023-10-12 11:52:46 -0400

    Made some updates to the settings.py file

--- ticketing_system/settings.py
+++ ticketing_system/settings.py
@@ -129,9 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
